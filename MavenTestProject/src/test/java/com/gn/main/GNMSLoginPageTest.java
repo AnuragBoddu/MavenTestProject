6e7e4494dc534b528f5e3abeb9ffa27b24ff626e
aboddu <aboddu@CLM-WIN7-ASGN09.portcullis.local>
2018-10-26 15:33:17 +0100

    First Commit

---
+++ MavenTestProject/src/test/java/com/gn/main/GNMSLoginPageTest.java
@@ -0,0 +1,47 @@
+package com.gn.main;
+
+import org.junit.After;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class GNMSLoginPageTest 
+{
+	
+	final String password = "Nov@2018";
+	private WebDriver driver;
+
+	@Before
+	public void setUp() throws Exception 
+	{
+		System.setProperty("webdriver.ie.driver","C:/Users/aboddu/Downloads/IEDriverServer_x64_3.14.0/IEDriverServer.exe");
+		driver = new InternetExplorerDriver();
+		driver.manage().window().maximize();
+	}
+
+
+	@Test
+	public void testGNMSTitlePage() throws InterruptedException
+	{
+		
+		driver.navigate().to("http://127.0.0.1:7001/gnms/");
+		String strPageTitle = driver.getTitle();
+		assertThat(strPageTitle).isEqualToIgnoringCase("G N M S");
+		
+		
+	}
+		
+	
+	@After
+	public void tearDown() 
+	{
+		if(driver!=null)
+		{
+			System.out.println("Closing IE browser");
+			driver.quit();
+		}
+	}
+}
